8ef2e5c3b2442e16673038d86e34ff19540bb1f6
roche-tan <rochelyn.rallestan@gmail.com>
2023-07-31 13:10:29 +0200

    Uploaded C04 ex02

---
+++ C04/ex02/ft_putnbr.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rosabari <rosabari@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/26 13:26:35 by rosabari          #+#    #+#             */
+/*   Updated: 2023/07/27 17:40:05 by rosabari         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		write (1, "-2147483648", 11);
+		return;
+	}
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		nb = -nb;
+	}
+	if (nb >= 10)
+	{
+		ft_putnbr (nb / 10);
+	}
+	ft_putchar('0' + nb % 10);
+}
+
+int	main(void)
+{
+	ft_putnbr(-235350);
+	return (0);
+}
