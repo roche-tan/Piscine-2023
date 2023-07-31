a6a923cc81ddb605561eeac05610fddf9791590a
roche-tan <rochelyn.rallestan@gmail.com>
2023-07-31 11:45:05 +0200

    Uploaded C01 ex04

---
+++ C01/ex04/ft_ultimate_div_mod.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_div_mod.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rosabari <rosabari@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/11 19:23:14 by rosabari          #+#    #+#             */
+/*   Updated: 2023/07/12 15:46:26 by rosabari         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdio.h>
+
+void	ft_ultimate_div_mod(int *a, int *b)
+{
+	int	div;
+	int	mov;
+
+	div = *a / *b;
+	mov = *a % *b;
+	*a = div;
+	*b = mov;
+}
+
+int main()
+{
+	int x;
+	int	y;
+
+	x = 15;
+	y = 3;
+	ft_ultimate_div_mod(&x, &y);
+	printf("%d | %d", x, y);
+}
