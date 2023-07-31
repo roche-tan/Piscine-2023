e0d10abaa2648ab738d72ea35acd82499a671308
roche-tan <rochelyn.rallestan@gmail.com>
2023-07-31 12:37:34 +0200

    Uploaded C02 ex07

---
+++ C02/ex07/ft_strupcase.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strupcase.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rosabari <rosabari@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/18 18:05:08 by rosabari          #+#    #+#             */
+/*   Updated: 2023/07/20 17:26:40 by rosabari         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdio.h>
+
+char	*ft_strupcase(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] -= 32;
+		}
+		i++;
+	}
+	return (str);
+}
+
+int main()
+{
+	char a[] = "fhjdhfjhfjhfkjdsf";
+	
+	printf("%s\n", ft_strupcase(a));
+	return (0);
+} 
