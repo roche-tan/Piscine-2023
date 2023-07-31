f46286965053651a4f2deb12a6e5441f55a75712
roche-tan <rochelyn.rallestan@gmail.com>
2023-07-31 13:12:20 +0200

    Uploaded C04 ex03

---
+++ C04/ex03/ft_atoi.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rosabari <rosabari@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/26 19:58:09 by rosabari          #+#    #+#             */
+/*   Updated: 2023/07/27 12:17:46 by rosabari         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdio.h>
+
+int	ft_atoi(char *str)
+{
+	int	sym;
+	int	num;
+
+	sym = 1;
+	num = 0;
+	while ((*str >= '\t' && *str <= '\r') || *str == ' ')
+		str++;
+	while (*str == '+' || *str == '-')
+	{
+		if (*str == '-')
+			sym *= -1;
+		str++;
+	}
+	while (*str >= '0' && *str <= '9')
+	{
+		num = (*str - '0') + (num * 10);
+		str++;
+	}
+	return (num * sym);
+}
+
+int	main(void)
+{
+	printf("%d",ft_atoi(" ---+--+1234ab567"));
+	return (0);
+}
+
