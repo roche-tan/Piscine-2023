aa857fac9bffa5905ae1ce2c45e5ed4d7390320e
roche-tan <rochelyn.rallestan@gmail.com>
2023-07-31 11:32:12 +0200

    Uploaded C01 ex00, ex01

---
+++ C01/ex01/ft_ultimate_ft.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_ft.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rosabari <rosabari@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/11 11:21:50 by rosabari          #+#    #+#             */
+/*   Updated: 2023/07/12 15:32:24 by rosabari         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_ultimate_ft(int *********nbr)
+{
+	*********nbr = 42;
+}
+
+int	main()
+{
+	int n;
+	int *nbr8;
+	int **nbr7;
+	int ***nbr6;
+	int ****nbr5;
+	int *****nbr4;
+	int ******nbr3;
+	int *******nbr2;
+	int ********nbr1;
+	int *********nbr;
+
+
+	n = 21;
+	
+	nbr8 = &n;
+	nbr7 = &nbr8;
+	nbr6 = &nbr7;
+	nbr5 = &nbr6;
+	nbr4 = &nbr5;
+	nbr3 = &nbr4;
+	nbr2 = &nbr3;
+	nbr1 = &nbr2;
+	nbr = &nbr1;
+	
+
+	ft_ultimate_ft(nbr);
+	printf("%d", n);	
+	return 0;
+}
