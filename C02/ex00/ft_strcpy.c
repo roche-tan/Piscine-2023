ca26cd3d79a31b1236eef7b3136c955aa86733bf
roche-tan <rochelyn.rallestan@gmail.com>
2023-07-31 11:57:50 +0200

    Uploaded C02 ex00

---
+++ C02/ex00/ft_strcpy.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rosabari <rosabari@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/12 16:12:54 by rosabari          #+#    #+#             */
+/*   Updated: 2023/07/21 13:08:05 by rosabari         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdio.h>
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+int main(void)
+{
+	char dest[] = "";
+
+	printf("%lu\n", strlen(dest));	
+	ft_strcpy(dest, "Super mega test!");
+	printf("%s", dest);	
+}
